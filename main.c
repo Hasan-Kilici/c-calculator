bbceecffb3f672d6b51914c815404e300c67241e
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-13 22:25:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,19 @@ int main()
 	long result;
 	char islem;
 	printf("calculator \n");
-	printf("Sum: +\nSub: - \nDivide: / \nmultiplication: * \n");
+	printf("Sum: + or 1\nSub: - or 2\nDivide: / or 3 \nmultiplication: * or 4\n");
 	scanf("%c",&islem);
   switch(islem){
     case '+':
+		printf("Sum \n");
+		printf("Please enter the first number: \t \a");
+		scanf("%d",&n1);
+		printf("Please enter the second number: \t \a");
+		scanf("%d",&n2);
+		result = n1 + n2;
+		printf("result = %d \n",result);
+    break;
+    case '1':
 		printf("Sum \n");
 		printf("Please enter the first number: \t \a");
 		scanf("%d",&n1);
@@ -28,7 +37,25 @@ int main()
 		result = n1 - n2;
 		printf("result = %d \n",result);
     break;
+    case '2':
+		printf("Sub \n");
+		printf("Please enter the first number: \t \a");
+		scanf("%d",&n1);
+		printf("Please enter the second number: \t \a");
+		scanf("%d",&n2);
+		result = n1 - n2;
+		printf("result = %d \n",result);
+    break;
     case '/':
+		printf("Divide \n");
+		printf("Please enter the first number: \t \a");
+		scanf("%d",&n1);
+		printf("Please enter the second number: \t \a");
+		scanf("%d",&n2);
+		result = n1 / n2;
+		printf("result = %d \n",result);
+    break;
+    case '3':
 		printf("Divide \n");
 		printf("Please enter the first number: \t \a");
 		scanf("%d",&n1);
@@ -46,10 +73,16 @@ int main()
 		result = n1 * n2;
 		printf("result = %d \n",result);
     break;
+    case '4':
+		printf("Multiplaction \n");
+		printf("Please enter the first number: \t \a");
+		scanf("%d",&n1);
+		printf("Please enter the second number: \t \a");
+		scanf("%d",&n2);
+		result = n1 * n2;
+		printf("result = %d \n",result);
+    break;
 	}
-	
-	
-	
 	return 0;
  }
 }
